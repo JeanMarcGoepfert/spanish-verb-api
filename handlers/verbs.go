72578e890212d1f100ea2c296d534ddca319d558
Jean-Marc Goepfert <goepfertjeanmarc@gmail.com>
2019-04-14 16:44:20 +1000

    Return 404 when verb not found

--- handlers/verbs.go
+++ handlers/verbs.go
@@ -4,11 +4,18 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"net/http"
+	"spanish-api/lib"
 	"spanish-api/repo"
 )
 
 func GetVerb(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	verb := repo.GetVerb(params["verb"])
+
+	if verb == nil {
+		lib.SetError(w, http.StatusNotFound, "Verb not found")
+		return
+	}
+
 	json.NewEncoder(w).Encode(verb)
 }
---
+++ lib/errors.go
@@ -0,0 +1,13 @@
+package lib
+
+import (
+	"encoding/json"
+	"net/http"
+	"spanish-api/models"
+)
+
+func SetError(w http.ResponseWriter, status int, message string) http.ResponseWriter {
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(models.Error{Message: message, Status: status})
+	return w
+}
